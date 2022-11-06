31a4dd61f5a1a7204476cb79009b5e33d16e0283
narendar2706 <62331095+narendar2706@users.noreply.github.com>
2022-11-07 01:12:29 +0530

    Create Q_2_4.cs

---
+++ Q_2_4.cs
@@ -0,0 +1,77 @@
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int id;
+        String accountType;
+        double balance, withdraw;
+
+        Account acc = new Account();
+
+        Console.WriteLine("Enter account id");
+        id = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter account type");
+        accountType = Console.ReadLine();
+
+        Console.WriteLine("Enter account balance");
+        balance = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter amount to withdraw");
+        withdraw = Convert.ToDouble(Console.ReadLine());
+
+        acc = new Account(id, accountType, balance);
+        Console.WriteLine(acc.GetDetails());
+
+        if (acc.WithDraw(withdraw))
+        {
+            Console.WriteLine("New Balance: " + acc.Balance);
+        }
+    }
+}
+public class Account
+{
+    private int id;
+    private string accountType;
+    private double balance;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string AccountType
+    {
+        get { return accountType; }
+        set { accountType = value; }
+    }
+    public double Balance
+    {
+        get { return balance; }
+        set { balance = value; }
+    }
+    public Account() { }
+    public Account(int id, string accountType, double balance)
+    {
+        this.id = id;
+        this.accountType = accountType;
+        this.balance = balance;
+    }
+
+    public bool WithDraw(double amount)
+    {
+        if (balance > amount)
+        {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+    public string GetDetails()
+    {
+        return ("Account Id: " + id + "\nAccount Type: " + accountType + "\nBalance: " + balance);
+    }
+}
