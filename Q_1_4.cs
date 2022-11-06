cc7875d394a29c092b6fbe70018841790bc92eeb
narendar2706 <62331095+narendar2706@users.noreply.github.com>
2022-11-07 01:10:16 +0530

    Create Q_1_4.cs

---
+++ Q_1_4.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+namespace cs_handsons
+{
+    class Q4
+    {
+        static void Main(string[] args)
+        {
+            sbyte number = 125;
+            Console.WriteLine($"Value of the number : {number}");
+            Console.WriteLine($"Largest value stored in a signed byte : {sbyte.MaxValue}");
+
+        }
+    }
+}
